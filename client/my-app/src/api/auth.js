6c80f6511d96ef312f3966ba0b2826cb5c2cec8a
jieun <bolouoeo0717@naver.com>
2025-08-29 10:33:11 +0900

    프로젝트 구조 추가

---
+++ client/my-app/src/api/auth.js
@@ -0,0 +1,11 @@
+// POST
+// /auth/login
+// 로그인
+
+// POST
+// /auth/logout
+// 로그아웃
+
+// POST
+// /auth/register
+// 회원가입
---
+++ client/my-app/src/api/diary.js
@@ -0,0 +1,7 @@
+// 특정 날짜 일기 조회 (YYYY-MM-DD)
+// /diary/${date}
+// GET
+
+// 일기 저장
+// /diary/${date}
+// POST
